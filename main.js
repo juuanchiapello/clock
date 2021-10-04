6286d629a95516e70edab4ba7c1fc19f2f14bb65
juuanchiapello <juuanchiapello@gmail.com>
2021-10-04 15:05:52 -0300

    hr ok

--- main.js
+++ main.js
@@ -12,10 +12,10 @@ document.addEventListener("DOMContentLoaded", function(){
     	var mm = day.getMinutes() * deg;
 		var ss = day.getSeconds() * deg;
 
-		hr.style.transform = `rotateZ (${hh + (mm / 12)} deg)`;
+		hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
 		mn.style.transform = `rotateZ(${mm}deg)`;
 		sc.style.transform = `rotateZ(${ss}deg)`;
 	});
+
 });
 	
-console.log(hr);
